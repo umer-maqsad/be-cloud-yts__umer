ea708563f92466c46c6cf5332db25e7945a3bd89
umer-maqsad <122520233+umer-maqsad@users.noreply.github.com>
2023-05-09 19:37:48 +0500

    [FEAT] Added DynamoDB tables from CDK :muscle:

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkStack extends cdk.Stack {
+export class StaticStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const movieTable = new dynamodb.Table(this, 'Movie', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableClass: dynamodb.TableClass.STANDARD,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
